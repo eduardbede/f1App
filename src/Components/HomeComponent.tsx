2cf5f2b6fcfc1401bbf3a60c89f9330e9e0e9781
eduardbede <bede.eduard@gmail.com>
2023-01-06 13:04:43 +0200

    update

--- src/Components/HomeComponent.tsx
+++ src/Components/HomeComponent.tsx
@@ -1,13 +1,19 @@
 import useDarkTheme from "./useDarkTheme"
 import './HomeComponent.css'
+import {motion} from 'framer-motion'
 
 export default function HomeComponent() {
   const {darkTheme}=useDarkTheme();
   return (
-    <div className={`textDivHome ${!darkTheme ? 'textDivHomeDark': ''}`}>Welcome to our Formula 1 website! Here you will find all the latest news, results, and analysis from the world of Formula 1 racing. 
+    <motion.div className={`textDivHome ${!darkTheme ? 'textDivHomeDark': ''}`}
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1 }}
+               
+    >Welcome to our Formula 1 website! Here you will find all the latest news, results, and analysis from the world of Formula 1 racing. 
       We cover every Grand Prix race and keep you up-to-date with the latest standings and points for all the drivers and teams. 
       Whether you are a die-hard fan or a newcomer to Formula 1, our site has something for everyone. 
       Stay with us for all the action from the fast-paced and exciting world of Formula 1!
-    </div>
+    </motion.div>
   )
 }
